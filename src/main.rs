bcbc68ad894f923a07c8aae39e5f5c91c10312a6
baiyuze <1347417308@qq.com>
2023-12-12 16:00:03 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
+use std::io;
+
 fn main() {
     println!("你好世界");
     let t = test();
-    print!("{}",t);
+    print!("{}", t);
     tuple();
-    structFn();
-    enumFn();
+    struct_fn();
+    enum_fn();
+    arr_fn();
+    process_fn();
 }
 
 fn test() -> String {
@@ -22,8 +26,8 @@ fn test() -> String {
     // f.insert(111, 1);
     for c in "草打哈哈".chars() {
         print!("{}", c);
-    };
-    
+    }
+
     // f.replace_range(range, replace_with)//替换的范围
     println!("{},{},{},{},{}", _a, _b, _c, f, ddd);
     // size_of_val(b)
@@ -38,46 +42,45 @@ fn test() -> String {
 /*  */
 
 fn tuple() {
-    let a = (3.1,4,5);
-    println!("{:?}",a.0);
+    let a = (3.1, 4, 5);
+    println!("{:?}", a.0);
 }
 
-fn structFn() {
+fn struct_fn() {
     #[derive(Debug)]
     struct User {
         name: String,
         age: u8,
         six: u16,
-        active: bool
+        active: bool,
     }
 
     let mut user = User {
         name: String::from("张三"),
         age: 20,
         six: 123,
-        active: true
+        active: true,
     };
-    
+
     let mut _user1 = User {
         name: String::from("张三"),
         ..user
     };
-    struct Color (i32,i32,i32);
+    struct Color(i32, i32, i32);
     // 元组结构体
-    let mut _tuple1 = Color(1,2,3);
+    let mut _tuple1 = Color(1, 2, 3);
     // user.age = 30;
     print!("{:?}", _user1);
     dbg!(_user1);
-    
 }
 
 // 枚举
-fn enumFn() {
+fn enum_fn() {
     #[derive(Debug)]
     enum IpAddr {
         HH,
         BB,
-        DD
+        DD,
     }
     let hh = IpAddr::HH;
     let dd = IpAddr::DD;
@@ -86,20 +89,76 @@ fn enumFn() {
     #[derive(Debug)]
 
     enum Tcp {
-        TcpBox(u32),
-        TLSBox(u64)
+        // TcpBox(u32),
+        TLSBox(u64),
     }
     let b = Tcp::TLSBox(11);
-        dbg!(b);
+    dbg!(b);
     #[derive(Debug)]
     enum Option<T> {
         Some(T),
-        None
+        None,
     }
-    let a = Value::null;
-    let absent_number: Option<String> = Option::None;
-        dbg!(absent_number);
-        print!("{}", a)
 
+    let absent_number: Option<String> = Option::Some(String::from("212"));
+    dbg!(absent_number);
+    // print!("{}", a)
+    let _a = [1, 2, 3];
+    let _b = [5; 5];
+    let a = [1, 2, 3, 4, 5];
+    // const BOX_TITLE: String = String::from("哈哈");
+    const _MAX_POINTS: u32 = 100_000; // 定义一个不可变的 u32 类型的常量 MAX_POINTS
+                                      // let mut index = String::new();
+                                      // 读取控制台的输出
+                                      // io::stdin()
+                                      //     .read_line(&mut index)
+                                      //     .expect("Failed to read line");
+                                      // let index: usize = index
+                                      //     .trim()
+                                      //     .parse()
+                                      //     .expect("报错");
+                                      // .parseInt()
+                                      // .expect("Index entered was not a number");
 
+    // let element = a[index];
+
+    // println!("值元素 index {} is: {}", index, element);
+}
+
+fn arr_fn() {
+    let arr: [String; 8] = std::array::from_fn(|_i| String::from("哈哈"));
+    println!("{:#?}", arr);
+    let s = &arr[1..3];
+    println!("{:#?}", s);
+}
+
+fn process_fn() {
+    let _condition = true;
+    let arr: [String; 8] = std::array::from_fn(|_i| String::from("哈哈"));
+
+    // 循环
+    for i in &arr {
+        println!("{:#?}111", i);
+    }
+    for j in 0..arr.len() {
+        if (j > 2) {
+            print!("{:#?}---", arr[j]);
+            continue;
+        } else {
+            break;
+        }
+    }
+    enum Box {
+        Test,
+        Npx,
+    }
+
+    // while arr.len() {
+
+    // }
+    // if _condition {
+    //     return 9;
+    // } else {
+    //     8
+    // }
 }
